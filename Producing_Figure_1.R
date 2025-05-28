9f9578922aeeaa6efcf8f5a31952ba32e131a012
ojovic985 <84029214+ojovic985@users.noreply.github.com>
2025-05-28 16:36:04 +0100

    Add files via upload

--- Producing_Figure_1.R
+++ Producing_Figure_1.R
@@ -1,124 +1,137 @@
+#R
 
-library(readxl)
-L<-read_excel("Supplementary_table_results_main.xlsx",sheet = "p025")
-L1<-as.numeric(as.matrix(L[3:38,7:197]))
-A1<-matrix(L1,nrow=36,ncol=191)
-L<-read_excel("Supplementary_table_results_main.xlsx",sheet = "p05")
-L1<-as.numeric(as.matrix(L[3:38,7:197]))
-A2<-matrix(L1,nrow=36,ncol=191)
-L<-read_excel("Supplementary_table_results_main.xlsx",sheet = "p10")
-L1<-as.numeric(as.matrix(L[3:38,7:197]))
-A3<-matrix(L1,nrow=36,ncol=191)
-L<-read_excel("Supplementary_table_results_main.xlsx",sheet = "p20")
-L1<-as.numeric(as.matrix(L[3:38,7:197]))
-A4<-matrix(L1,nrow=36,ncol=191)
-L<-read_excel("Supplementary_table_results_main.xlsx",sheet = "p30")
-L1<-as.numeric(as.matrix(L[3:38,7:197]))
-A5<-matrix(L1,nrow=36,ncol=191)
-L<-read_excel("Supplementary_table_results_main.xlsx",sheet = "p50")
-L1<-as.numeric(as.matrix(L[3:38,7:197]))
-A6<-matrix(L1,nrow=36,ncol=191)
-L<-read_excel("Supplementary_table_results_main.xlsx",sheet = "p70")
-L1<-as.numeric(as.matrix(L[3:38,7:197]))
-A7<-matrix(L1,nrow=36,ncol=191)
-L<-read_excel("Supplementary_table_results_main.xlsx",sheet = "p80")
-L1<-as.numeric(as.matrix(L[3:38,7:197]))
-A8<-matrix(L1,nrow=36,ncol=191)
-
-F1<-rbind(A1,A2,A3,A4,A5,A6,A7,A8)
-
-T<-matrix(rep(NA, nrow(F1)*63),nrow= nrow(F1))
-
-for (i in 1:nrow(T)) {
-for (j in 1:ncol(T)) {
-T[i,j]<-F1[i,j+65]/F1[i,j+128]
-}}
-any(is.na(T))
+#Working directory in R must be '/A_NA_FPro'
 
 
+######## EXTREMELY IMPORTANT: First run: "Preparing_input_for_producing_figure_1.R" - THEN continue by running this!
 
-T<--T #Comment: This change in sign is only necessary because in our Main code we put: m_diff[ji]<-mean(Anem[,ji])-mean(NonAnem[,ji]) instead of   m_diff[ji]<-mean(NonAnem[,ji])-mean(Anem[,ji])
+## By default we will set:
 
-Zu<-as.vector(t(colMeans(T)))
-EM<-array(ncol(T))
-medi<-array(ncol(T))
-mini<-array(ncol(T))
-maxi<-array(ncol(T))
-quart1<-array(ncol(T))
-quart3<-array(ncol(T))
+source("Preparing_input_for_producing_figure_1.R")
 
-for (i in 1:ncol(T)) {
-EM[i]<-1.968*sd(T[,i])/sqrt(nrow(T))
-medi[i]<-median(T[,i])
-mini[i]<-quantile(T[,i])[1]
-quart1[i]<-quantile(T[,i])[2]
-quart3[i]<-quantile(T[,i])[4]
-maxi[i]<-quantile(T[,i])[5]
-}
+A<-read.csv("Food_classes_A_perc_sorted_by_Fpro_G2404_without_M1.csv")
+A<-A[,-1]
+A<-A[,-1]
 
+De<-read.csv("Correlation_Fpro_vs_Score_for_17_cat.csv",header=TRUE)
+De<-De[,-1]
+De<-as.vector(t(De))
 
-FG<-c("prot","fat","carb","alco","mois","caff","theo","sugr","fibe","calc","iron","magn","phos","pota","sodi","zinc","copp","sele","reti","bcar","acar","atoc","vd","cryp","lyco","lz","vc","vb1","vb2","niac","vb6","fola","vb12","tchl","vk","fa","tff","atoa","b12a","chol","sfat","s040","s060","s080","s100","s120","s140","s160","s180","m181","p182","p183","p204","p226","m161","p184","m201","p205","m221","p225","mfat","pfat","Fpro")
-color<-c("red","darkgreen")
+WE<-read.csv("Compas_scores_Added_to_2404_without_M1_All_food_codes_from_WWEIA_analysis_Incl_dup.csv")
+WE<-WE[,-1]
+Fpro<-WE[,"min_FPro"]
+Food_Compass_Score_2<-WE[,ncol(WE)]
 
-FG2<-c("Protein","Total fat","Carbohydrate","Alcohol","Water", "Caffeine", "Theobromine","Total sugars","Dietary fiber", "Calcium", "Iron", "Magnesium", "Phosphorus", "Potassium", "Sodium", "Zinc","Copper","Selenium","Retinol","β-carotene", "α-carotene", "Vitamin E", "Vitamin D", "β-cryptoxanthin", "Lycopene", "Lut.+zexanthin","Vitamin C","Vitamin B1","Vitamin B2", "Vitamin B3", "Vitamin B6","Total folate", "Vitamin B12", "Choline","Vitamin K", "Folic acid", "Food folate", "Add. vit. E","Add. vit. B12", "Cholesterol", "Total SFA", "SFA 4:0", "SFA 6:0", "SFA 8:0", "SFA 10:0", "SFA 12:0", "SFA 14:0", "SFA 16:0", "SFA 18:0", "MFA 18:1", "PFA 18:2", "PFA 18:3", "PFA 20:4","PFA 22:6", "MFA 16:1", "PFA 18:4","MFA 20:1","PFA 20:5","MFA 22:1", "PFA 22:5","Total MFA","Total PFA","Fpro")
+De<-as.vector(t(cbind(t(cor(Fpro,Food_Compass_Score_2)),t(De))))
 
-mdif<-Zu
+#dev.new(width=15.86, height=10.7, unit="in")
 
-dev.new(width=15.86, height=10.2, unit="in")
+png("Figure_1.png", width = 6600, height = 3450, res = 450)
+par(mar = c(0, 0, 0, 1),cex=0.8)
 frame()
-plot.window(xlim=c(0,20), ylim=c(0,16))
+plot.window(xlim=c(-0.5,2.6), ylim=c(-13.2,1.5))
+#text(0.7,1.5,"Consumption of food categories by anemic and non-anemic participants",cex=1.4)
 
-text(10,16.25,"Average standardized difference in means (95% C.I.) between NA and A through all datasets",cex=1.4)
+segments(x0=0.1,y0=1.2,x1=0.1,y1=-11.5,lwd=1)
+segments(x0=0.1,y0=-11.42,x1=1.3,y1=-11.42)
+
+for (i in 0:10) {
+segments(x0=0.1+i*0.12,y0=1.2,x1=0.1+i*0.12,y1=-11.5)
+text(0.1+i*0.12,-11.7,paste(i*10,"%"))
+}
+expression(italic("Non") * "-" * italic("A"))
+
+library(plotrix)
+
+text(-0.03,1.7,"Food Category",cex=1.2,pos=2)
+text(-0.03,0.9,"Overall",cex=1.2,pos=2)
+text(0.1,1.7,expression(italic(n)),cex=1.2,pos=2)
+text(0.1,0.9,paste(sum(A[,3])+sum(A[,4])),cex=1.2,pos=2)
+text(0.1+1.2*0.2,1.7,expression(italic(f) * "(" * italic(A) * ")"),cex=1.2)
+text(0.1+1.2*0.8,1.7,expression(italic(f) * "(" * italic("Non") * "-" * italic("A") * ")"),cex=1.2)
+text(1.7+0.4,1.7,"FPro",cex=1.2)
+text(1.12+0.4,1.7,expression(italic(R) * "(FPro vs. Score)"),cex=1.2)
+
+rectFill(x1=0.1, y1=0.9-0.2, x2=0.1+sum(A[,4])/(sum(A[,3])+sum(A[,4]))*1.2, y2=0.9+0.2, fg="pink", bg = "pink",pch=0,pch.col="pink")
+rectFill(x1=0.1+sum(A[,4])/(sum(A[,3])+sum(A[,4]))*1.2, y1=0.9-0.2, x2=1.3, y2=0.9+0.2, fg="cyan", bg = "cyan",pch=0,pch.col="cyan")
+text(0.1+0.2*1.2,0.9,paste(round(100*sum(A[,4])/(sum(A[,3])+sum(A[,4])),1) ) )
+text(0.1+0.8*1.2,0.9,paste(round(100*sum(A[,3])/(sum(A[,3])+sum(A[,4])),1) ) )
 
 
-segments(x0=0.8,y0=10,x1=20,y1=10)
-text(0,10,"m([m(NA)-m(A)]/EM(95% CI))",cex=1.3,srt=90,xpd=TRUE)
-segments(x0=0.8,y0=1.9,x1=0.8,y1=16)
 
-segments(x0=0.7,y0=10,x1=0.8,y1=10)
-segments(x0=0.7,y0=12,x1=0.8,y1=12)
-segments(x0=0.7,y0=14,x1=0.8,y1=14)
-segments(x0=0.7,y0=16,x1=0.8,y1=16)
-segments(x0=0.7,y0=8,x1=0.8,y1=8)
-segments(x0=0.7,y0=6,x1=0.8,y1=6)
-segments(x0=0.7,y0=4,x1=0.8,y1=4)
-segments(x0=0.7,y0=2,x1=20,y1=2)
-segments(x0=0.8,y0=8,x1=20,y1=8,lty="dashed",col="purple")
-segments(x0=0.8,y0=12,x1=20,y1=12,lty="dashed",col="purple")
-text(0.48,16,"3",cex=1.3)
-text(0.48,14,"2",cex=1.3)
-text(0.48,12,"1",cex=1.3)
-text(0.48,10,"0",cex=1.3)
-text(0.48,8,"-1",cex=1.3)
-text(0.48,6,"-2",cex=1.3)
-text(0.48,4,"-3",cex=1.3)
-text(0.48,2,"-4",cex=1.3)
-tuf<-c(12,63)
-
-for (i in 1:63) {
-if (i%in%tuf) {
-colo<-color[2]}
-else {
-colo<-color[1]}
-points(0.9+0.3*i,10+2*mdif[i],pch=16,col="blue",cex=1.2)
-#text(0.9+0.3*i,1.25,paste(FG[i]),cex=1.2,srt=90,xpd=TRUE)
-text(1.05+0.3*i,1.77,paste(FG2[i]),cex=1.2,srt=90,xpd=TRUE,pos=2)
-segments(x0=0.9+0.3*i,y0=1.9,x1=0.9+0.3*i,y1=2)
-segments(x0=0.9+0.3*i,y0=10+2*mini[i],x1=0.9+0.3*i,y1=10+2*quart1[i],col=colo,lwd=2)
-segments(x0=0.9+0.3*i,y0=10+2*quart3[i],x1=0.9+0.3*i,y1=10+2*maxi[i],col=colo,lwd=2)
-segments(x0=0.9+0.3*i+0.1,y0=10+2*quart1[i],x1=0.9+0.3*i+0.1,y1=10+2*quart3[i],col=colo,lwd=2)
-segments(x0=0.9+0.3*i-0.1,y0=10+2*quart1[i],x1=0.9+0.3*i-0.1,y1=10+2*quart3[i],col=colo,lwd=2)
-
-segments(x0=0.9+0.3*i-0.1,y0=10+2*quart1[i],x1=0.9+0.3*i+0.1,y1=10+2*quart1[i],col=colo,lwd=2)
-
-segments(x0=0.9+0.3*i-0.1,y0=10+2*quart3[i],x1=0.9+0.3*i+0.1,y1=10+2*quart3[i],col=colo,lwd=2)
-segments(x0=0.9+0.3*i-0.1,y0=10+2*medi[i],x1=0.9+0.3*i+0.1,y1=10+2*medi[i],col=colo,lwd=2)
-segments(x0=0.9+0.3*i,y0=2,x1=0.9+0.3*i,y1=10+2*mini[i],col="gray",lwd=1,lty="dashed")
-
-segments(x0=0.9+0.3*i-0.1,y0=10+2*(mdif[i]-EM[i]),x1=0.9+0.3*i+0.1,y1=10+2*(mdif[i]-EM[i]),col="orange",lwd=2)
-segments(x0=0.9+0.3*i-0.1,y0=10+2*(mdif[i]+EM[i]),x1=0.9+0.3*i+0.1,y1=10+2*(mdif[i]+EM[i]),col="orange",lwd=2)
+for (i in 1:nrow(A)) {
+text(-0.03,0.9-0.7*i,paste(A[i,1]),cex=1.2,pos=2)
+text(0.1,0.9-0.7*i,paste(A[i,3]+A[i,4]),cex=1.2,pos=2)
+rectFill(x1=0.1, y1=0.9-0.2-0.7*i, x2=0.1+(A[i,4]/(A[i,3]+A[i,4]))*1.2, y2=0.9+0.2-0.7*i, fg="pink", bg = "pink",pch=0,pch.col="pink")
+rectFill(x1=0.1+(A[i,4]/(A[i,3]+A[i,4]))*1.2, y1=0.9-0.2-0.7*i, x2=1.3, y2=0.9+0.2-0.7*i, fg="cyan", bg = "cyan",pch=0,pch.col="cyan")
+text(0.1+0.2*1.4,0.9-0.7*i,paste(round(100*A[i,4]/(A[i,3]+A[i,4]),1) ) )
+text(0.1+0.8*1.4,0.9-0.7*i,paste(round(100*A[i,3]/(A[i,3]+A[i,4]),1) ) )
 }
 
 
+rectFill(x1=0.95-0.3, y1=-12.4, x2=1.002-0.3, y2=-12.0, fg="pink", bg = "pink",pch=0,pch.col="pink")
+rectFill(x1=0.9, y1=-12.4, x2=0.952, y2=-12.0, fg="cyan", bg = "cyan",pch=0,pch.col="cyan")
+text(1.071-0.3,-12.23,expression(italic(A)),cex=1.2,pos=2)
+text(1.399-0.3,-12.23,expression(italic("Non") * "-" * italic("A")),cex=1.2,pos=2)
+
+segments(x0=1.7,y0=1.2,x1=1.7,y1=-11.5,lwd=1)
+segments(x0=1.7,y0=-11.42,x1=2.6,y1=-11.42)
+for (i in 0:10) {
+segments(x0=1.7+i*0.09,y0=1.2,x1=1.7+i*0.09,y1=-11.5)
+text(1.7+i*0.09,-11.7,paste(i/10))
+}
+
+rectFill(x1=1.7, y1=0.9, x2=1.7+0.7586938*0.9, y2=0.9+0.2, fg="red", bg = "red",pch=0,pch.col="red")
+segments(x0=1.7+(0.7586938-0.01496126)*0.9,y0=0.9+0.1,x1=1.7+(0.7586938+0.01496126)*0.9,y1=0.9+0.1,col="black")
+segments(x0=1.7+(0.7586938-0.01496126)*0.9,y0=0.9,x1=1.7+(0.7586938-0.01496126)*0.9,y1=0.9+0.2,col="black")
+segments(x0=1.7+(0.7586938+0.01496126)*0.9,y0=0.9,x1=1.7+(0.7586938+0.01496126)*0.9,y1=0.9+0.2,col="black")
+rectFill(x1=1.7, y1=0.9-0.2, x2=1.7+0.6764192*0.9, y2=0.9, fg="darkgreen", bg = "darkgreen",pch=0,pch.col="darkgreen")
+segments(x0=1.7+(0.6764192-0.01825134)*0.9,y0=0.9-0.1,x1=1.7+(0.6764192+0.01825134)*0.9,y1=0.9-0.1,col="black")
+segments(x0=1.7+(0.6764192-0.01825134)*0.9,y0=0.9,x1=1.7+(0.6764192-0.01825134)*0.9,y1=0.9-0.2,col="black")
+segments(x0=1.7+(0.6764192+0.01825134)*0.9,y0=0.9,x1=1.7+(0.6764192+0.01825134)*0.9,y1=0.9-0.2,col="black")
+
+
+for (i in 1:nrow(A)) {
+rectFill(x1=1.7, y1=0.9-0.7*i, x2=1.7+A[i,8]*0.9, y2=0.9-0.7*i+0.2, fg="red", bg = "red",pch=0,pch.col="red")
+segments(x0=1.7+(A[i,8]-A[i,11])*0.9,y0=0.9+0.1-0.7*i,x1=1.7+(A[i,8]+A[i,11])*0.9,y1=0.9+0.1-0.7*i,col="black")
+segments(x0=1.7+(A[i,8]-A[i,11])*0.9,y0=0.9-0.7*i,x1=1.7+(A[i,8]-A[i,11])*0.9,y1=0.9+0.2-0.7*i,col="black")
+segments(x0=1.7+(A[i,8]+A[i,11])*0.9,y0=0.9-0.7*i,x1=1.7+(A[i,8]+A[i,11])*0.9,y1=0.9+0.2-0.7*i,col="black")
+rectFill(x1=1.7, y1=0.9-0.2-0.7*i, x2=1.7+A[i,9]*0.9, y2=0.9-0.7*i, fg="darkgreen", bg = "darkgreen",pch=0,pch.col="darkgreen")
+segments(x0=1.7+(A[i,9]-A[i,12])*0.9,y0=0.9-0.1-0.7*i,x1=1.7+(A[i,9]+A[i,12])*0.9,y1=0.9-0.1-0.7*i,col="black")
+segments(x0=1.7+(A[i,9]-A[i,12])*0.9,y0=0.9-0.7*i,x1=1.7+(A[i,9]-A[i,12])*0.9,y1=0.9-0.2-0.7*i,col="black")
+segments(x0=1.7+(A[i,9]+A[i,12])*0.9,y0=0.9-0.7*i,x1=1.7+(A[i,9]+A[i,12])*0.9,y1=0.9-0.2-0.7*i,col="black")}
+
+rectFill(x1=2.18, y1=-12.4, x2=2.232, y2=-12.0, fg="red", bg = "red",pch=0,pch.col="red")
+rectFill(x1=1.9, y1=-12.4, x2=1.952, y2=-12.0, fg="darkgreen", bg = "darkgreen",pch=0,pch.col="darkgreen")
+text(2.099,-12.23,expression(italic("Non") * "-" * italic("A")),cex=1.2,pos=2)
+text(2.295,-12.23,expression(italic(A)),cex=1.2,pos=2)
+
+
+segments(x0=1.4,y0=1.2,x1=1.4,y1=-11.5,lwd=1)
+segments(x0=1.52,y0=1.2,x1=1.52,y1=-11.5,lwd=1)
+segments(x0=1.64,y0=1.2,x1=1.64,y1=-11.5,lwd=1)
+segments(x0=1.4,y0=-11.42,x1=1.64,y1=-11.42)
+text(1.4,-11.7,"-1")
+text(1.52,-11.7,"-0.5")
+text(1.64,-11.7,"0")
+
+rectFill(x1=1.64+De[1]*(1.64-1.4), y1=0.9-0.2, x2=1.64, y2=0.9+0.2, fg="orange", bg = "orange",pch=0,pch.col="orange")
+
+for (i in 1:nrow(A)) {
+rectFill(x1=1.64+De[1+i]*(1.64-1.4), y1=0.9-0.2-0.7*i, x2=1.64, y2=0.9+0.2-0.7*i, fg="orange", bg = "orange",pch=0,pch.col="orange")
+}
+
+
+segments(x0=1.67,y0=-12.07,x1=1.67,y1=0.5,lty="dashed",col="purple")
+segments(x0=1.62,y0=0.5,x1=1.72,y1=0.5,lty="dashed",col="purple")
+segments(x0=1.66,y0=-12.07+0.2,x1=1.67,y1=-12.07,lty="dashed",col="purple")
+segments(x0=1.68,y0=-12.07+0.2,x1=1.67,y1=-12.07,lty="dashed",col="purple")
+text(1.47,-12.31,expression("Increasing " * italic(m) * "(FPro)"),cex=1.1,pos=4,col="purple")
+segments(x0=2.34,y0=-12.2,x1=2.4,y1=-12.2)
+segments(x0=2.34,y0=-12.05,x1=2.34,y1=-12.35)
+segments(x0=2.4,y0=-12.05,x1=2.4,y1=-12.35)
+text(2.4,-12.23,"95% CI",cex=1.1,pos=4,col="black")
+
+dev.off()
 
 
